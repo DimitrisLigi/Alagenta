d310cf26da3484476fd42e947fd1bede896a9e1a
unknown <ikalkavouris@yahoo.com>
2022-07-27 23:54:18 +0300

    Fix axriasta comments

--- app/src/main/java/com/dimitrisligi/alagenta/SignUpActivity.kt
+++ app/src/main/java/com/dimitrisligi/alagenta/SignUpActivity.kt
@@ -12,7 +12,7 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         // the onCreate function
-        //AMXXMXMXMXMXMXMXMX
+        // Note: Please, do NOT add useless comments
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
