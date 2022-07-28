e854ace66f87af6edfbb6c4f94607c0d84cee881
Dimitris Ligkitsakos <dimitrisligi@gmail.com>
2022-07-28 18:18:22 +0300

    Version 0.0002 Toasting the date when pressing the calendar

--- app/src/main/java/com/dimitrisligi/alagenta/MainCalendarActivity.kt
+++ app/src/main/java/com/dimitrisligi/alagenta/MainCalendarActivity.kt
@@ -2,10 +2,29 @@ package com.dimitrisligi.alagenta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.dimitrisligi.alagenta.databinding.ActivityMainCalendarBinding
 
 class MainCalendarActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainCalendarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_calendar)
+        mBinding = ActivityMainCalendarBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        mBinding.calendarView
+        setupCalendar()
+
     }
+
+    private fun setupCalendar() {
+        mBinding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val mYear = year
+            val mMonth = month
+            val MDom = dayOfMonth
+            //Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
+            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
+            Toast.makeText(this@MainCalendarActivity, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
